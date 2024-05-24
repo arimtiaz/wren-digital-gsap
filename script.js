a6f8c6a44606b3e2ba27cb618748b2947578fc5c
Abadur Rahman Imtiaz <abadur391@gmail.com>
2024-05-24 06:01:57 +0000

    Text Animations Sorted

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-gsap.from(".heading-1", { x: -200 });
+window.addEventListener("DOMContentLoaded", (event) => {
+  let typeSplit = new SplitType("[text-split]", {
+    types: "words, chars",
+    tagName: "span",
+  });
+
+  // Link timelines to scroll position
+  function createScrollTrigger(triggerElement, timeline) {
+    // Reset tl when scroll out of view past bottom of screen
+    ScrollTrigger.create({
+      trigger: triggerElement,
+      start: "top bottom",
+      onLeaveBack: () => {
+        timeline.progress(0);
+        timeline.pause();
+      },
+    });
+
+    ScrollTrigger.create({
+      trigger: triggerElement,
+      start: "top 60%",
+      onEnter: () => timeline.play(),
+    });
+  }
+
+  $("[letters-slide-up]").each(function (index) {
+    let tl = gsap.timeline({ paused: true });
+    tl.from($(this).find(".char"), {
+      yPercent: 100,
+      duration: 0.2,
+      ease: "power1.out",
+      stagger: { amount: 0.6 },
+    });
+    createScrollTrigger($(this), tl);
+  });
+
+  gsap.set("[text-split]", { opacity: 1 });
+});
